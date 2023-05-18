a33889f085a211af05b501f22a5a9dfe28014f02
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-05-19 04:21:23 +0600

    toy detiails by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 app.use(cors())
@@ -24,6 +24,22 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const toyCollection = client.db('turboThriller').collection('toys')
+
+        app.get('/toys', async (req, res) => {
+            const result = await toyCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/toyDetails/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.findOne(query)
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
