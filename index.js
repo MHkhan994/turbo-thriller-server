a496db2283aa11e2a05dae0c0e4cab8194ff3496
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-05-20 02:35:56 +0600

    sort by name

--- index.js
+++ index.js
@@ -27,11 +27,35 @@ async function run() {
 
         const toyCollection = client.db('turboThriller').collection('toys')
 
+        // sends all toys by all users
         app.get('/toys', async (req, res) => {
             const result = await toyCollection.find().toArray()
             res.send(result)
         })
 
+        // sends the total length of toys
+        app.get('/totalToys', async (req, res) => {
+            const result = await toyCollection.estimatedDocumentCount();
+            res.send({ totalToys: result })
+        })
+
+        // sends data considering limit
+        app.get('/toyLimit', async (req, res) => {
+            const page = req.query.page
+            const skip = (page - 1) * 20
+            const result = await toyCollection.find().skip(skip).limit(20).toArray()
+            res.send(result)
+        })
+
+        // sends data sorting by toy name
+        app.get('/toyName', async (req, res) => {
+            const name = req.query.name;
+            const searchRegex = name.split(' ').map((word) => new RegExp(word, 'i'));
+            const result = await toyCollection.find({ $or: searchRegex.map((regex) => ({ name: regex })) }).toArray()
+            res.send(result)
+        })
+
+        // sends single toy
         app.get('/toyDetails/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
@@ -39,6 +63,7 @@ async function run() {
             res.send(result)
         })
 
+        // sends toys that match user email
         app.get('/myToys/:email', async (req, res) => {
             const email = req.params.email;
             const query = { seller_email: email }
@@ -46,12 +71,14 @@ async function run() {
             res.send(result)
         })
 
+        // adds a new toy to database
         app.post('/toys', async (req, res) => {
             const toy = req.body
             const result = await toyCollection.insertOne(toy)
             res.send(result)
         })
 
+        // deletes single toy
         app.delete('/myToys/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
@@ -59,6 +86,7 @@ async function run() {
             res.send(result)
         })
 
+        // updates single toy
         app.patch('/myToys/:id', async (req, res) => {
             const id = req.params.id;
             const toy = req.body;
@@ -70,7 +98,7 @@ async function run() {
                     price: toy.price,
                     quantity: toy.quantity,
                     subcategory: toy.subcategory,
-                    description: toy.description
+                    description: toy.description,
                 }
             }
             const result = await toyCollection.updateOne(filter, updateToy)
